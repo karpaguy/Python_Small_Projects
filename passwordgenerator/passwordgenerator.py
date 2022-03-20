2914f503c0f13b4480789f72da1128aeda8f1c1a
unknown <denuzzidouglas@gmail.com>
2022-03-20 00:11:07 -0300

    options

--- passwordgenerator/passwordgenerator.py
+++ passwordgenerator/passwordgenerator.py
@@ -1,12 +1,14 @@
 import random
 from string import octdigits
 
+# Ainda falta determinar o total de caracteres que deseja na senha.
+
 def generate():
   password = ""
   sn = True # Confirma os dados anteriores e segue
   if sn == True:
     for letter in range(10):
-      letnum = random.randrange(1) # Decide se será 1 ou 2.
+      letnum = random.randrange(2) # Decide se será 1 ou 2.
       if letnum == 1: # 1 é letra.
         character = random.randrange(97, 122)
         character = chr(character)
@@ -46,29 +48,36 @@ def salvar(password): # Listei o parâmetro sendo a varíavel password, existent
   if sn2 == "sim":
     passname = input("\nDigite um nome para esta senha: ")
     passwords_file = open("mypasswords.txt", "a")
-    passwords_file.write("\n"+ passname + ": " + password)
+    passwords_file.write("• " + passname + ": " + password + "\n")
     passwords_file.close()
-  elif sn2 == "não": # Código empaca aqui, mesmo se for uma outra coisa.
+  elif sn2 == "não": # Descarta a senha.
     print("Senha descartada")
   else: # Retorna a pergunta se for digitado outro valor para o input.
     print("Este não é um comando válido.")
-    salvar()
+    salvar(password) # É preciso especificar o argumento novamente para iniciar sem erros.
+
+def verifysaves():
+  passwords_file = open("mypasswords.txt", "r")
+  print(passwords_file.readlines())
+  passwords_file.close()
+  op2 = input("Deseja trocar de opção (op) ou fechar o programa (fechar)? ")
+  op2 = op2.lower()
+  if op2 == "op": # Retorna para opções.
+    options()
+  elif op2 == "fechar": # Fecha o programa.
+    print("O programa será fechado.")
+    exit
+  else:
+    print("Este não é um comando válido.")
+    verifysaves()
 
 def options():
   op = input("Deseja fazer uma nova senha (nova), ver suas senhas salvas (salvas) ou fechar o programa (fechar)? ")
   op = op.lower()
   if op == "nova": # Gera senhas.
     generate()
-  elif op == "salvas": # Abre o arquivo e mostra ele na linha de código - DEVE EXISTIR JUNTO DA PASTA.
-    passwords_file = open("mypasswords.txt", "r")
-    passwords_file.read() # Falta ajustes para ficar correto.
-    passwords_file.close()
-    op2 = input("Deseja trocar de opção (op) ou fechar o programa (fechar)? ")
-    if op2 == "op": # Retorna para opções.
-      options()
-    elif op2 == "fechar": # Fecha o programa.
-      print("O programa será fechado.")
-      exit
+  elif op == "salvas": # Abre o arquivo e mostra o conteúdo na linha do terminal - DEVE EXISTIR JUNTO DA PASTA.
+    verifysaves()
   elif op == "fechar": # Fecha o programa.
     print("O programa será fechado.")
     exit
